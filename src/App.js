52a7a076293605c861914dbe2a84fb82f805ea5d
Anton Vasylchenko <87824792+Smd16Bi@users.noreply.github.com>
2023-06-04 07:54:09 +0300

    push git

--- src/App.js
+++ src/App.js
@@ -1,6 +1,72 @@
+import React, { useState } from "react";
+
 const App = () => {
+
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ["/", "*", "+", "-", "."];
+
+  const updateCalc = (value) => {
+    if ( 
+      ops.includes(value) && calc ===  "" ||
+      ops.includes(value) && ops.includes(calc.slice(-1))
+      ) {
+      return;
+    }
+    setCalc(calc + value);
+
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString() );
+    }
+  }
+
+  const calculate = () => {
+     setCalc(eval(calc).toString())
+  }
+
+  const delateLast = () => {
+    if (calc === "") {
+      return
+    }
+
+    const value = calc.slice(0, -1);
+    setCalc(value)
+  }
+
+  const createDigits = () => {
+    const digits = [];
+    for (let i = 1; i <= 9; i++) {
+      digits.push(
+        <button onClick={() => updateCalc(i.toString())} key={i}>{i}</button>
+      );
+    }
+    return digits;
+  }
   return (
-    <div>Hello</div>
+    <div className="App">
+      <div className="calculator">
+        <div className="dispay">
+          {result ? <span>({result})</span> : ""}
+          {calc || "0"}
+        </div>
+        <div className="operators">
+          <button onClick={() => updateCalc("/")}>/</button>
+          <button onClick={() => updateCalc("*")} >*</button>
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+
+          <button onClick={delateLast}>DEL</button>
+        </div>
+
+        <div className="digits">
+          {createDigits()}
+          <button onClick={() => updateCalc("0")}>0</button>
+          <button onClick={() => updateCalc(".")}>.</button>
+          <button onClick={calculate}>=</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
